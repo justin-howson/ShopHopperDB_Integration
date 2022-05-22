b8d867af082f8a50db66e74887433881a164171f
justin-howson <71288176+justin-howson@users.noreply.github.com>
2022-05-22 13:56:11 -0700

    added wix scrapers and started on weebly and square

---
+++ shop-hopper-db/pages/api/wixAPI-scraper/index.js
@@ -0,0 +1,22 @@
+import { main } from '../../../backend/WixAPI';
+import prisma from '../../../prisma/prisma.js';
+
+
+export default async (req, res) => {
+    try {
+        const { business_name, product_type } = req.body;
+        const data = await main(business_name, product_type);
+
+        const result = await prisma.product.createMany({
+            data: data,
+            skipDuplicates: true
+        });
+
+
+
+        return res.status(200).json({ result });
+    } catch (error) {
+        console.error('error:', error);
+        return res.status(500).json(error);
+    }
+};
---
+++ shop-hopper-db/pages/square-weebly.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const square_weebly = () => {
+    const [isFetching, setIsFetching] = useState(false);
+    const [numberReceived, setNumberReceived] = useState(0);
+    const [itemsRemoved, setItemsRemoved] = useState(false);
+
+    const uploadProducts = async () => {
+        setItemsRemoved(false);
+        setIsFetching(true);
+        const response = await fetch('/api/square&weebly-scraper', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        setIsFetching(false);
+
+        const result = await response.json();
+        console.log('/square&weebly.js - result: ', result);
+        // const num = res.result.length;
+        setNumberReceived(result?.result?.count);
+    };
+
+    const removeProducts = async (business_name) => {
+        setNumberReceived(0);
+        const response = await fetch('/api/square&weebly/removeProducts', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        const result = await response.json();
+        if (result.status === 'success') {
+            setItemsRemoved(true);
+        }
+    };
+
+    return (
+        <Wrapper>
+            <Left>
+                <h1>Square and Weebly Scraper</h1>
+                <p>Get Products will scrape all Square and Weebly Products and place their data in the Shophopper Database</p>
+                {isFetching && <div>Getting products...</div>}
+                <Div>
+                    <button onClick={() => uploadProducts()}>Get Products</button>
+
+                    <Right>
+                        <p fontSize={16}>{isFetching && 'Scraping Products from Square and Weebly Websites'}</p>
+
+                        {numberReceived > 0 && <p fontSize={16}>{`${numberReceived} items added to database`}</p>}
+                    </Right>
+                </Div>
+                <p>Clear Products will remove all Square and Weebly products from the database</p>
+                <Div>
+                    <button onClick={() => removeProducts()}>Clear Products</button>
+                    <Right>
+                        <p fontSize={16}>{itemsRemoved && 'All Square and Weebly Products removed from Database'}</p>
+                    </Right>
+                </Div>
+            </Left>
+        </Wrapper>
+    );
+};
+
+export default square_weebly;
+// ---------------------------STYLES-------------------------------------------//
+
+const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    position: relative;
+    gap: 25px;
+    padding: 6%;
+    color: grey;
+`;
+
+const Left = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+    p-align: left;
+    margin-left: -10px;
+    z-index: 5;
+    position: relative;
+    padding: 20px;
+    gap: 15px;
+`;
+
+const Right = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 100px;
+    width: 250px;
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    align-items: center;
+    padding-top: 33px;
+    color: #65a4b8;
+`;
+const Div = styled.div`
+    height: 100px;
+    position: relative;
+`;
